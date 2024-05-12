06ebd568694c0e6adb879f15f67d4d829b37c8aa
Lucas Giolo <giololucas@gmail.com>
2024-05-12 12:54:10 -0300

    :sparkles: feat: Adiciona as rotas na aplicação principal

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import 'express-async-errors';
 
 import errorMiddleware from './middlewares/errorMiddleware';
+import { teamRouter, loginRouter, matchesRouter } from './routes';
 
 class App {
   public app: express.Express;
@@ -13,6 +14,8 @@ class App {
 
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
+
+    this.routes();
 
     // Não remova esse middleware de erro, mas fique a vontade para customizá-lo
     // Mantenha ele sempre como o último middleware a ser chamado
@@ -33,6 +36,12 @@ class App {
 
   public start(PORT: string | number): void {
     this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+
+  private routes(): void {
+    this.app.use(teamRouter);
+    this.app.use(loginRouter);
+    this.app.use(matchesRouter);
   }
 }
 
