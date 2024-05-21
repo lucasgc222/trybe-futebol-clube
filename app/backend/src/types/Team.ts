b5d6346122e9a51f785b2cf5ca3e7bd23d92f339
Lucas Giolo <giololucas@gmail.com>
2024-05-21 13:35:51 -0300

    :recycle: refactor: Altera TeamPoints para ser uma interface

--- app/backend/src/types/Team.ts
+++ app/backend/src/types/Team.ts
@@ -1,14 +1,14 @@
-export type TeamPoints = {
-  name: string,
-  totalPoints: number,
-  totalGames: number,
-  totalVictories: number,
-  totalDraws: number,
-  totalLosses: number,
-  goalsFavor: number,
-  goalsOwn: number,
-  goalsBalance: number,
-  efficiency: string,
-};
+export interface TeamPoints {
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  efficiency: string;
+}
 
 export type TeamSide = 'Home' | 'Away';
