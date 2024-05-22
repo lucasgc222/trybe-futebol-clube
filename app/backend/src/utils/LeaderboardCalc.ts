70c440886a2e2fbb63c3d9366b5f600033c0188e
Lucas Giolo <giololucas@gmail.com>
2024-05-21 22:26:57 -0300

    :sparkles: feat: Cria função para pontuação geral dos times

--- app/backend/src/utils/LeaderboardCalc.ts
+++ app/backend/src/utils/LeaderboardCalc.ts
@@ -30,6 +30,13 @@ class LeaderboardCalc {
     return info;
   }
 
+  private static sortTeamList(teamList: TeamPoints[]): TeamPoints[] {
+    return teamList.sort((a, b) => b.totalPoints - a.totalPoints
+    || b.totalVictories - a.totalVictories
+    || b.goalsBalance - a.goalsBalance
+    || b.goalsFavor - a.goalsFavor);
+  }
+
   getLeaderboardByType(side: TeamSide): TeamPoints[] {
     const teamPoints = this.teamsMatches.map((team) => {
       const matches = side === 'Home' ? team.homeMatches : team.awayMatches;
@@ -42,11 +49,26 @@ class LeaderboardCalc {
     return LeaderboardCalc.sortTeamList(teamPoints);
   }
 
-  private static sortTeamList(teamList: TeamPoints[]): TeamPoints[] {
-    return teamList.sort((a, b) => b.totalPoints - a.totalPoints
-    || b.totalVictories - a.totalVictories
-    || b.goalsBalance - a.goalsBalance
-    || b.goalsFavor - a.goalsFavor);
+  getLeaderboard(): TeamPoints[] {
+    const teamPoints = this.teamsMatches.map((team) => {
+      const homePoints = LeaderboardCalc.calculatePoints('Home', team.homeMatches);
+      const awayPoints = LeaderboardCalc.calculatePoints('Away', team.awayMatches);
+      const points = new TeamScore();
+
+      points.name = team.teamName;
+      points.totalGames = homePoints.totalGames + awayPoints.totalGames;
+      points.totalVictories = homePoints.totalVictories + awayPoints.totalVictories;
+      points.totalDraws = homePoints.totalDraws + awayPoints.totalDraws;
+      points.totalLosses = homePoints.totalLosses + awayPoints.totalLosses;
+      points.totalPoints = homePoints.totalPoints + awayPoints.totalPoints;
+      points.goalsFavor = homePoints.goalsFavor + awayPoints.goalsFavor;
+      points.goalsOwn = homePoints.goalsOwn + awayPoints.goalsOwn;
+      points.goalsBalance = homePoints.goalsBalance + awayPoints.goalsBalance;
+      points.efficiency = ((points.totalPoints / (points.totalGames * 3)) * 100).toFixed(2);
+
+      return points;
+    });
+    return LeaderboardCalc.sortTeamList(teamPoints);
   }
 }
 
