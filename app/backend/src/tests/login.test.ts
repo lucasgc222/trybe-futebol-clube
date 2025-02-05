393e69055f04a5f497586ec698a479fa4c7949cf
Lucas Giolo <giololucas@gmail.com>
2025-02-05 15:57:20 -0300

    fix(test): Corrige importação em testes
    Corrige importação para usar sintaxe do ES6 (Erro apontado pelo workflow do github)

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,7 +1,8 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import 'mocha';
 // @ts-ignore
-import chaiHttp = require('chai-http');
+import * as chaiHttp from 'chai-http';
 
 import { app } from '../app';
 
--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,7 +1,7 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
-import chaiHttp = require('chai-http');
+import * as chaiHttp from 'chai-http';
 
 import { app } from '../app';
 
